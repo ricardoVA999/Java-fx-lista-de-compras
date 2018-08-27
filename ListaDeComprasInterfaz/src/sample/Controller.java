353fd88aa0565e8208e5067c839075ec3e09041e
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2018-08-26 21:02:28 -0600

    Add files via upload

--- ListaDeComprasInterfaz/src/sample/Controller.java
+++ ListaDeComprasInterfaz/src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class Controller {
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    public void Openwindow(ActionEvent event){
+        Parent root;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NuevaLista.fxml"));
+            root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Nueva Lista");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e){
+            System.out.println("Cant load new window");
+        }
+    }
 }
