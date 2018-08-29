c04a13890929354c86c3844fa9eae13ff35bd280
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2018-08-28 20:47:58 -0600

    Add files via upload

---
+++ ListaDeComprasInterfaz/src/sample/Clases/Item.java
@@ -0,0 +1,48 @@
+package sample.Clases;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Item {
+    SimpleStringProperty nombre;
+    SimpleIntegerProperty cantidad;
+    SimpleDoubleProperty precio;
+    SimpleBooleanProperty estado;
+    public Item(String nombre, int cantidad, double precio){
+        this.nombre = new SimpleStringProperty();
+        this.cantidad = new SimpleIntegerProperty();
+        this.precio = new SimpleDoubleProperty();
+        this.estado = new SimpleBooleanProperty();
+
+        this.nombre.set(nombre);
+        this.cantidad.set(cantidad);
+        this.precio.set(precio);
+        this.estado.set(false);
+    }
+    public String getNombre() {
+        return this.nombre.get();
+    }
+    public void setNombre(String fName) {
+        this.nombre.set(fName);
+    }
+    public int getCantidad() {
+        return this.cantidad.get();
+    }
+    public void setCantidad(int fcantidad) {
+        this.cantidad.set(fcantidad);
+    }
+    public double getPrecio() {
+        return this.precio.get();
+    }
+    public void setNombre(double fPrecio) {
+        this.precio.set(fPrecio);
+    }
+    public Boolean getEstado() {
+        return this.estado.get();
+    }
+    public void setEstado(Boolean fEstado) {
+        this.estado.set(fEstado);
+    }
+}
